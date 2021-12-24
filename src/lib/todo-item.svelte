838584c49889a5afd59fa451bc81a89da8472e67
Suresh Kandru <kandru.suresh@gmail.com>
2021-12-24 19:12:17 +0000

    Developed HTML Structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form action="" method="post">
+        <input type="hidden" name="done" value=""/>
+        <button aria-label="Mark done/not done">Done/Not Done</button>
+    </form>
+
+    <form action="" method="">
+        <input type=""/>
+        <button aria-label="Save todo">Save</button>
+    </form>
+
+    <form action="" method="post">
+        <button aria-label="Delete">Delete</button>
+    </form>
+</div>
